5cb57e1babd17cd9428663154c417fd9c29a2dcf
Jeet Prajapati <jeetprajapati1617@gmail.com>
2026-02-28 12:31:19 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const ejs = require('ejs');
 const app = express();
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
 const connectDB = require("./config/db");
+const user = require("./modles/user")
 
 const PORT = 3000;
 app.use(express.json());
@@ -11,10 +13,14 @@ connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});     
+}); 
+
+app.post("./",(req,res)=>{
+  res.send('Hello');
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-module.exports = bodyparser;
+module.exports = bodyparser , user;
